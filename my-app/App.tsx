ba00d4f68afeb3ee5e9a347dad7aa1795e3c3a80
khalidh7 <kabouelrous@gmail.com>
2024-10-19 11:52:00 -0700

    create employee info page

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import EmployeeInfo from './pages/EmployeeInfo';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App: React.FC = () => {
+  const employeeData = {
+    name: 'Employee Name',
+    phoneNumber: '(123) 456 - 7890',
+    email: 'Email@email.com',
+  };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const handleBackPress = () => {
+    console.log('Back button pressed');
+  };
+
+  return <EmployeeInfo employee={employeeData} onBackPress={handleBackPress} />;
+};
+
+export default App;
