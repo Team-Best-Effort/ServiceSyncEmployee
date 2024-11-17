82164f40aa8cdaac089f5566d6b6e583add9086f
khalidh7 <kabouelrous@gmail.com>
2024-11-17 10:01:04 -0800

    navigation for employee app

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,38 +1,24 @@
 import React from 'react';
 import EmployeeWorksiteInformation from './pages/EmployeeWorksiteInformation';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import EmployeeHome from './pages/EmployeeHome';
+import EmployeeInfo from './pages/EmployeeInfo';
 
 const App: React.FC = () => {
-  const worksiteData = {
-    address: {
-      street: '1234 Example Street',
-      city: 'Vacaville',
-      state: 'CA',
-      zipCode: '94533',
-    },
-    tasks: [
-      { id: 1, title: 'Install new equipment' },
-      { id: 2, title: 'Test system functionality' },
-      { id: 3, title: 'Document installation' },
-    ],
-    deadline: 'December 31, 2024',
-  };
+ 
+  const Stack = createStackNavigator();
 
-  const handleGetDirections = () => {
-    console.log('getting directions');
-  };
-
-  const handleClockIn = () => {
-    console.log('clocking in');
-  };
 
   return (
-    <EmployeeWorksiteInformation
-      address={worksiteData.address}
-      tasks={worksiteData.tasks}
-      deadline={worksiteData.deadline}
-      onGetDirections={handleGetDirections}
-      onClockIn={handleClockIn}
-    />
+
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="EmployeeHome" screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="EmployeeHome" component={EmployeeHome} />
+          <Stack.Screen name="EmployeeInfo" component={EmployeeInfo} />
+          <Stack.Screen name="EmployeeWorksiteInformation" component={EmployeeWorksiteInformation} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 };
 
