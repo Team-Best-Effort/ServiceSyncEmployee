da47dd56e96cdf3447d7a830a80930b4a6f4b109
khalidh7 <kabouelrous@gmail.com>
2025-05-02 09:33:46 -0700

    fixes for the editprofile tests

--- my-app2/app/(dashboard)/profile/lib/firebase.ts
+++ my-app2/app/(dashboard)/profile/lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getDatabase } from 'firebase/database';
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -15,5 +16,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig, "service_sync");
 const db = getDatabase(app);
+export const auth = getAuth(app);
 
 export { db };
